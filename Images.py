bfd8d666f09abd64a14e06445e3cc4408aa11923
TenJlblu <dudya19999@gmail.com>
2018-12-17 13:40:47 +0500

    hellokkk

---
+++ Images.py
@@ -0,0 +1,14 @@
+import pygame
+
+class ImageBank:
+    Images = None
+
+    def __init__(self):
+        self.Images = []
+
+    def append(self, image):
+        self.Images.append(image)
+        return len(self.Images) - 1
+
+    def dell_by_index(self, index):
+        del self.Images[index]
