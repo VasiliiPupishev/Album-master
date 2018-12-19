25657448a4b40795199c9047040d425255b5fc3c
TenJlblu <dudya19999@gmail.com>
2018-12-20 00:47:20 +0500

    hellokkk

--- venv/AddAlbum.py
+++ venv/AddAlbum.py
@@ -18,10 +18,7 @@ class AddAlbum:
        loading_caption = font.render("Creating new album", False, (0, 0, 0))
        screen.blit(background_image, (0, 0))
        screen.blit(loading_caption, (380, 10))
-       font = pygame.font.SysFont('arial', 15)  # name caption
-       #loading_caption = font.render("Is this folder", False, (0, 0, 0))  # grouping in albums
-       #screen.blit(loading_caption, (410, 450))
-       # ---------------------------------------------------------------------------------------------------------------
+       font = pygame.font.SysFont('arial', 15)
        button = pygame.image.load("Backgrounds/plate.png")
        font = pygame.font.SysFont('arial', 20)
        button = pygame.transform.scale(button, (100, 40))
@@ -36,9 +33,6 @@ class AddAlbum:
        font = pygame.font.SysFont('arial', 25)
        loading_caption = font.render(self.Name, False, (0, 0, 0))  # grouping in albums
        screen.blit(loading_caption, (300, 250))
-       #button = pygame.image.load("Backgrounds/sq.png")
-       #button = pygame.transform.scale(button, (19, 19))
-       #screen.blit(button, (500, 450))
 
    def run_adding(self, root, screen):
        self.Name = ""
@@ -61,17 +55,6 @@ class AddAlbum:
 
    def search_events(self, pos, screen, root):
        x, y = pos
-       #if 500 < x < 520:
-       #    if 450 < y < 470:
-       #        if not self.Flag:
-       #            self.Flag = True
-       #            button = pygame.image.load("Backgrounds/back.png")
-       #            button = pygame.transform.scale(button, (19, 19))
-       #            button = pygame.transform.rotate(button, 90)
-       #            screen.blit(button, (500, 450))
-       #        else:
-       #            self.Flag = False
-       #            self.print_addition_menu(screen)
        if 200 < x < 800:
            if 230 < y < 300:
                if not self.Write:
@@ -81,7 +64,9 @@ class AddAlbum:
                if root.get_current_album(False) is not None:
                    if len(self.Name) == 0:
                        self.Name = "default"
-                   root.get_current_album(False).add_item(Album(self.Name, root.get_current_album(False).Name))
+                   newa = Album(self.Name, root.get_current_album(False).Name)
+                   newa.Index = 0
+                   root.get_current_album(False).add_item(newa)
                return True
            if 350 < x < 450:
                return True
